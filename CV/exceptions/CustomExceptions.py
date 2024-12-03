7c016326f7e9bc1cd48446307a7f1d211ed2cf1c
melarozz <melarozz18@gmail.com>
2024-12-03 21:35:46 +0700

    fix: linter fix

--- CV/exceptions/CustomExceptions.py
+++ CV/exceptions/CustomExceptions.py
@@ -17,6 +17,7 @@ class SaveError(CVException):
     """Raised when there is an error saving the OCR output."""
     pass
 
+
 class BlurryTextError(CVException):
     """Raised when there is a blurry text on image what makes the results of OCR very poor."""
     pass
