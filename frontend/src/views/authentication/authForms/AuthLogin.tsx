f5930a97813055f96ca43d8a2f24bc19e4e35894
Vova Petrov <v.petrov3@g.nsu.ru>
2025-03-22 16:37:29 +0700

    update login form

--- frontend/src/views/authentication/authForms/AuthLogin.tsx
+++ frontend/src/views/authentication/authForms/AuthLogin.tsx
@@ -33,8 +33,8 @@ const AuthLogin = ({ title, subtitle, subtext }: loginType) => {
 
   const formik = useFormik({
     initialValues: {
-      email: 'demo@demo.com',
-      password: 'demo123',
+      email: '',
+      password: '',
       submit: null,
     },
 
